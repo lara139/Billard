59abeff84afbbbd2e89f079b555933e98c8c3889
Lara SCHWITZGABEL <lara.schwitzgabel@supdevinci-edu.fr>
2025-04-02 10:11:49 +0200

    feat/ debug

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ io.on('connection', (socket) => {
   console.log('Player connected:', socket.id)
 
   socket.on('joinGame', (playerName) => {
+    console.log(`Player ${playerName} (${socket.id}) is attempting to join a game.`)
     gameState.players[socket.id] = {
       id: socket.id,
       name: playerName,
@@ -35,8 +36,10 @@ io.on('connection', (socket) => {
         socket.join(roomId)
         gameState.players[socket.id].room = roomId
         joinedRoom = true
-        
+        console.log(`Player ${playerName} (${socket.id}) joined room ${roomId}.`)
+
         if (room.players.length === 2) {
+          console.log(`Room ${roomId} is now full. Starting game.`)
           io.to(roomId).emit('gameStart', {
             players: room.players.map(id => gameState.players[id])
           })
@@ -53,24 +56,31 @@ io.on('connection', (socket) => {
       }
       socket.join(roomId)
       gameState.players[socket.id].room = roomId
+      console.log(`Player ${playerName} (${socket.id}) created and joined new room ${roomId}.`)
     }
   })
 
   socket.on('shot', (data) => {
     const room = gameState.players[socket.id]?.room
     if (room) {
+      console.log(`Player ${socket.id} shot with force ${data.force} in room ${room}.`)
       socket.to(room).emit('playerShot', {
         playerId: socket.id,
         force: data.force
       })
+    } else {
+      console.log(`Player ${socket.id} attempted to shoot but is not in a room.`)
     }
   })
 
   socket.on('disconnect', () => {
+    console.log(`Player disconnected: ${socket.id}`)
     const room = gameState.players[socket.id]?.room
     if (room) {
+      console.log(`Player ${socket.id} left room ${room}.`)
       gameState.rooms[room].players = gameState.rooms[room].players.filter(id => id !== socket.id)
       if (gameState.rooms[room].players.length === 0) {
+        console.log(`Room ${room} is now empty and has been deleted.`)
         delete gameState.rooms[room]
       } else {
         io.to(room).emit('playerLeft', socket.id)
